0c9236a391847f963cbecf86f52e74c11601f463
Peter Gerasimov <petergerasimov2000@gmail.com>
2020-03-19 15:41:50 +0200

    Fixed an oopsie

--- mini-project/employee.cpp
+++ mini-project/employee.cpp
@@ -14,13 +14,13 @@ Employee::Employee(const char* firstName,
                    const char* position
                   )                         
 {
-    this->firstName = new char[sizeof(firstName) + 1];
+    this->firstName = new char[strlen(firstName) + 1];
     strcpy(this->firstName, firstName);
 
-    this->lastName = new char[sizeof(lastName) + 1];
+    this->lastName = new char[strlen(lastName) + 1];
     strcpy(this->lastName, lastName);
 
-    this->position = new char[sizeof(position) + 1];
+    this->position = new char[strlen(position) + 1];
     strcpy(this->position, position);
 }
 
@@ -35,21 +35,21 @@ void Employee::setFirstName(const char* firstName)
 {
     if(this->firstName)
         delete[] this->firstName;
-    this->firstName = new char[sizeof(firstName) + 1];
+    this->firstName = new char[strlen(firstName) + 1];
     strcpy(this->firstName, firstName);
 }
 void Employee::setLastName(const char* lastName)
 {
     if(this->lastName)
         delete[] this->lastName;
-    this->lastName = new char[sizeof(lastName) + 1];
+    this->lastName = new char[strlen(lastName) + 1];
     strcpy(this->lastName, lastName);
 }
 void Employee::setPosition(const char* position)
 {
     if(this->position)
         delete[] this->position;
-    this->position = new char[sizeof(position) + 1];
+    this->position = new char[strlen(position) + 1];
     strcpy(this->position, position);
 }
 const char* Employee::getFirstName()
